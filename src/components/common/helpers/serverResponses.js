77424fed8787ca28198bb6a4c2662108180e19e1
alejandrodk <ottoabarriosp@hotmail.com>
2020-12-18 23:50:35 -0300

    Feature: Create HTTP responses helper

---
+++ src/components/common/helpers/serverResponses.js
@@ -0,0 +1,29 @@
+/**
+ * Handler error messages for Api responses
+ * @param {String} message
+ * @param {Number} status HTTP Status code
+ * @param {String} reason
+ */
+export class Failure {
+    constructor({ reason, message, status }) {
+      this.reason = reason;
+      this.message = message;
+      this.status = status;
+      this.date = new Date().toISOString();
+    }
+  }
+  /**
+   * Handler Successfully messages for Api responses
+   * @param {Number} status HTTP Status code
+   * @param {String} reason
+   * @param {Object} data
+   */
+  export class Success {
+    constructor({ message, status, data }) {
+      this.message = message;
+      this.status = status;
+      this.date = new Date().toISOString();
+      this.data = data;
+    }
+  }
+  

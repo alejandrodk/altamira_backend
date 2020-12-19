b83426f5203f4d4d173289578f7a53a679a3fa8e
alejandrodk <ottoabarriosp@hotmail.com>
2020-12-19 00:05:02 -0300

    Feature: Update prettier config file

--- src/components/common/helpers/serverResponses.js
+++ src/components/common/helpers/serverResponses.js
@@ -5,25 +5,24 @@
  * @param {String} reason
  */
 export class Failure {
-    constructor({ reason, message, status }) {
-      this.reason = reason;
-      this.message = message;
-      this.status = status;
-      this.date = new Date().toISOString();
-    }
+  constructor({ reason, message, status }) {
+    this.reason = reason;
+    this.message = message;
+    this.status = status;
+    this.date = new Date().toISOString();
   }
-  /**
-   * Handler Successfully messages for Api responses
-   * @param {Number} status HTTP Status code
-   * @param {String} reason
-   * @param {Object} data
-   */
-  export class Success {
-    constructor({ message, status, data }) {
-      this.message = message;
-      this.status = status;
-      this.date = new Date().toISOString();
-      this.data = data;
-    }
+}
+/**
+ * Handler Successfully messages for Api responses
+ * @param {Number} status HTTP Status code
+ * @param {String} reason
+ * @param {Object} data
+ */
+export class Success {
+  constructor({ message, status, data }) {
+    this.message = message;
+    this.status = status;
+    this.date = new Date().toISOString();
+    this.data = data;
   }
-  
+}
